83b55229c4f38702b31ea117414abb4ce4a26e43
Elon Gliksberg <elongliks@gmail.com>
2019-11-01 10:07:11 +0200

    Changed Gengar path.

--- Gengar/config.h
+++ Gengar/config.h
@@ -2,4 +2,4 @@ constexpr auto CNC_HOST = "127.1";
 constexpr auto CNC_PORT = 27016;
 constexpr auto TASK_NAME = "ChromeStatistics";
 constexpr auto RECONNECT_TO_CNC_INTERVAL_IN_SECONDS = 30;
-constexpr auto GENGAR_PATH = "C:\\Windows\\Temp\\ChromeDiagnose.log";
+constexpr auto GENGAR_PATH = "C:\\Windows\\Temp\\ChromeDiagnose";
